a5c81071d28a0f787f371f2f7962f968ead8b0fc
MinhyeSim <smine0032@gmail.com>
2025-03-13 01:02:48 +0900

    홈 컴포넌트 작업

---
+++ src/components/Router.tsx
@@ -0,0 +1,28 @@
+import {Route, Routes, Navigate} from "react-router-dom";
+import Home from "../pages/home";
+import LoginPage from "../pages/login";
+import PostList from "../pages/post";
+import PostDetail from "../pages/post/detail";
+import PostEdit from "../pages/post/edit";
+import PostNew from "../pages/post/new";
+import ProfilePage from "../pages/profile";
+import SignupPage from "../pages/signup";
+
+
+export default function Router(){
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/posts" element={<PostList/>}/>
+                <Route path="/posts/:id" element={<PostDetail />}/>
+                <Route path="/posts/new" element={<PostNew />}/>
+                <Route path="/posts/edit/:id" element={<PostEdit />}/>
+                <Route path="/profile" element={<ProfilePage />}/>
+                <Route path="/login" element={<LoginPage />}/>
+                <Route path="/signup" element={<SignupPage />}/>
+                <Route path="*" element={<Navigate replace to="/"/>}/>
+            </Routes>
+        </>
+    );
+}
