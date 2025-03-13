077b9662ee099033ab7b6a8214668f56a3b42947
MinhyeSim <smine0032@gmail.com>
2025-03-13 23:23:22 +0900

    form 컴포넌트 작업

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,17 +1,33 @@
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 interface PostListProps {
     hasNavigation? : boolean;
 }
 
+type TabType = "all" | "my";
+
 export default function PostList({hasNavigation = true }: PostListProps) {
+    const [activeTab, setActiveTab] = useState<TabType>("all");
+    
     return (
         <>
         {hasNavigation && (
-                    <div className="post__navigation">
-                    <div className="post__navigation--active">전체</div>
-                    <div>나의 글</div>
-                </div>
+        <div className="post__navigation">
+            <div 
+                role="presentation" 
+                onClick={() => setActiveTab("all")}
+                className={activeTab === "all" ? "post__navigation--active":""}
+            >
+            전체
+            </div>
+            <div
+                role="presentation" 
+                onClick={() => setActiveTab("my")}
+                className={activeTab === "my" ? "post__navigation--active":""}            
+            >
+            나의 글</div>
+        </div>
         )}
         <div className="post__list">
         {[...Array(10)].map((e, index)=>(
