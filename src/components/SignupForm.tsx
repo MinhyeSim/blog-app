0b9ec53dc9631505ac721016a061a49941f69131
MinhyeSim <smine0032@gmail.com>
2025-03-19 11:17:58 +0900

    context api로 사용자 정보 관리하기

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { app } from "firebaseApp";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
